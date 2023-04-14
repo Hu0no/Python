2f341e006aa9fc731d6118d6a1a4a8bc595d4eea
Hu0no <126472671+Hu0no@users.noreply.github.com>
2023-04-14 19:03:57 -0300

    Update Aula7.py

--- Aula7.py
+++ Aula7.py
@@ -41,3 +41,87 @@ while True:
     else:
         listai.append(n)
 ------------------------------------------------------------------------
+Tarefa 6:
+ lista = []
+while True:
+    n = int(input("Número: "))
+    if n< 0:
+        break
+    elif n not in lista:
+        lista.append(n)
+print(f"Sua lista de números sem repetição é: {lista}")
+--------------------------------------------------------------------------
+Tarefa 7: *
+    lista = []
+i = 0
+
+while i < 5:
+    a = float(input("Altura: "))
+    p = float(input("Peso: "))
+    lista.append((a, p))
+
+    tupla_igual = False
+    for x in range(i):
+            tupla_igual = True
+            break
+
+    if tupla_igual:
+        print("Pelo menos duas pessoas têm a mesma altura e peso.")
+        break
+
+    i += 1
+
+if not tupla_igual:
+    print("Não há pessoas com peso e alturas iguais")
+------------------------------------------------------------------------
+Tarefa 8: ***
+lista = []
+i = 0
+while i < 5:
+    a = float(input("Altura: "))
+    p = float(input("Peso: "))
+    lista += [(a,p)]
+    tupla_igual = False
+    for x in range (len(lista)): 
+        if  x in range(i):
+            tupla_igual = True
+            break
+    i+=1
+if tupla_igual == True:
+    print("Pelo menos duas pessoas têm a mesma altura e peso.")
+else:
+    print("Não há pessoas com peso e alturas iguais")
+-------------------------------------------------------------------------
+Tarefa 9:
+lista = [2,4,1,11,111, 5, 8, 9, 4]
+new_lista = []
+for x in lista:
+    if x in [2, 5, 8, 9, 4]:
+        new_lista.append(x)
+print(f"Lista com números 2, 5, 8, 9, 4: {new_lista}")
+-------------------------------------------------------------------------
+Tarefa 10:
+lista = [22,15,68,99,63]
+qtd = 0
+for e in lista:
+    if e % 2 == 0:
+        qtd+=1
+print(qtd)
+--------------------------------------------------------------------------
+Tarefa 11:
+nome = input("Nome: ")
+soma = 0
+vogal = "aeiouAEIOU"
+for letra in nome:
+    if letra in vogal:
+        soma+=1
+print(f"Seu nome possui {soma} vogais")
+--------------------------------------------------------------------------
+Tarefa 12:
+for i in range(0,101):
+    if i%2==0:
+        print(i, end=' ')
+ Exemplo 2:
+list = [print(e, end =' ') for e in range(2,101,2) if True]
+---------------------------------------------------------------------------
+Livro automate the boring stuff
